676a33c11c7b5d719d4be99c7b426ae72f35650c
Suresh S <sureshfizzy0503@gmail.com>
2024-01-05 20:06:19 +0530

    Adjust Paddings and Add some decorations

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -15,21 +15,32 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(25.0),
+      padding: EdgeInsets.only(left: 25.0, top: 25.0),
       child: Container(
         padding: EdgeInsets.all(24),
-        child: Row(
-          children: [
-            //checkbox
-            Checkbox(value: taskCompleted, onChanged: onChanged),
-
-            //task name
-            Text(taskName),
-          ],
-        ),
         decoration: BoxDecoration(
           color: Colors.yellow[500],
           borderRadius: BorderRadius.circular(14)),
+        child: Row(
+          children: [
+            //checkbox
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+              ),
+
+            //task name
+            Text(
+              taskName,
+              style: TextStyle(
+                decoration: taskCompleted
+                ? TextDecoration.lineThrough
+                : TextDecoration.none,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
